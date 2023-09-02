653cf42e0a277d785baa19d811c91fa5ae62d5b1
Kevin Low <kevin@lowones.com>
2023-09-02 05:59:57 -0400

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Kevin Low on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
